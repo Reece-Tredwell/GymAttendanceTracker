746384a457eb0c77bcf99de29b2a1b589b935a44
Reece <r.tredwell@outlook.com>
2025-07-12 18:29:00 +1000

    begun creating the back-end properly, started on building the register feature, created and hosted pgsql db on virtual machine

---
+++ backend/controllers/userAuthController.js
@@ -0,0 +1,43 @@
+const { Client } = require('pg');
+const fs = require('fs');
+const rawdata = fs.readFileSync('./config.json', 'utf8');
+const jsonData = JSON.parse(rawdata);
+const DBLoginInfo = jsonData["DBLogin"]
+const apiKeys = jsonData["api-keys"]
+
+function ConnectToDB(DBLoginInfo) {
+    const client = new Client({
+        user: DBLoginInfo["username"],
+        host: DBLoginInfo["host"],
+        database: DBLoginInfo["DBName"],
+        password: DBLoginInfo["password"],
+        port: DBLoginInfo["port"],
+    });
+
+
+    client.connect()
+        .then(() => console.log('Connected to PostgreSQL'))
+        .catch(err => console.error('Connection error', err));
+
+    return client
+}
+
+
+exports.register = async (req, res) => {
+    const submittedAPIKey = req.header("api-key")
+    if (submittedAPIKey != apiKeys["auth"]) {
+        res.send("Invalid API Key")
+        return
+    }
+    client = ConnectToDB(DBLoginInfo)
+    const email = req.body.email
+    const password_hash = req.body.password_hash
+    const queryInsert = `INSERT INTO production.users (email, password_hash) VALUES ($1, $2)`;
+    await client.query(queryInsert, [email, password_hash]);
+    res.send('Connected To DB');
+};
+
+exports.login = (req, res) => {
+    res.send('logging ');
+
+};
---
+++ backend/routes/userAuth.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const userAuthController = require('../controllers/userAuthController');
+
+router.post('/register', userAuthController.register);
+
+router.post('/login', userAuthController.login);
+
+module.exports = router;
