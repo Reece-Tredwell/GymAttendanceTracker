5dcf66975389255bef80d5c1ad1c7726e2b48ada
Reece <r.tredwell@outlook.com>
2025-07-15 22:36:10 +1000

    programmed the back-end to now use HTTPS, the server utilises NGROK which allows for the api to be passed via HTTPS (secure API call)

--- backend/controllers/userAuthController.js
+++ backend/controllers/userAuthController.js
@@ -1,4 +1,5 @@
 const { Client } = require('pg');
+const passwordHash = require('password-hash');
 const fs = require('fs');
 const rawdata = fs.readFileSync('./config.json', 'utf8');
 const jsonData = JSON.parse(rawdata);
@@ -24,17 +25,32 @@ function ConnectToDB(DBLoginInfo) {
 
 
 exports.register = async (req, res) => {
+    fetch("http://localhost:8181/auth/registerHiddenLogic", { //NGROK forwards the request to the local IP, this process allows for HTTPS (secured body)
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'api-key': apiKeys["auth"]
+        },
+        body: JSON.stringify({ email: req.body.email, password_hash: passwordHash.generate(req.body.password_hash) })
+    })
+    res.send('Connected To DB');
+};
+
+
+exports.registerHiddenLogic = async (req, res) => {
+    console.log("here")
     const submittedAPIKey = req.header("api-key")
     if (submittedAPIKey != apiKeys["auth"]) {
         res.send("Invalid API Key")
         return
-    }    
+    }
     client = ConnectToDB(DBLoginInfo)
     const email = req.body.email
     const password_hash = req.body.password_hash
     const queryInsert = `INSERT INTO production.users (email, password_hash) VALUES ($1, $2)`;
     await client.query(queryInsert, [email, password_hash]);
-    res.send('Connected To DB');
+    await client.end();
+    res.send('Inserted Into DB');
 };
 
 exports.login = (req, res) => {
--- backend/routes/userAuth.js
+++ backend/routes/userAuth.js
@@ -4,6 +4,8 @@ const userAuthController = require('../controllers/userAuthController');
 
 router.post('/register', userAuthController.register);
 
+router.post('/registerHiddenLogic', userAuthController.registerHiddenLogic);
+
 router.post('/login', userAuthController.login);
 
 module.exports = router;
