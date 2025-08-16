77eae755f59020cfc3bb07b6cdf7a2913fc0be52
Reece <r.tredwell@outlook.com>
2025-08-16 12:38:40 +1000

    started work on the addDate endpoint

--- backend/controllers/gymController.js
+++ backend/controllers/gymController.js
@@ -1,5 +1,29 @@
-exports.addDate = (req, res) => {
-  res.send('Date added');
+const { Client } = require('pg');
+const fs = require('fs');
+const rawdata = fs.readFileSync('./config.json', 'utf8');
+const jsonData = JSON.parse(rawdata);
+const DBLoginInfo = jsonData["DBLogin"]
+
+function ConnectToDB(DBLoginInfo) {
+    const client = new Client({
+        user: DBLoginInfo["username"],
+        host: DBLoginInfo["host"],
+        database: DBLoginInfo["DBName"],
+        password: DBLoginInfo["password"],
+        port: DBLoginInfo["port"],
+    });
+    client.connect()
+
+
+    return client
+}
+
+exports.addDate = async(req, res) => {
+  client = ConnectToDB(DBLoginInfo)
+  const sessionToken = req.body.sessionToken
+  const queryInsert = `SELECT * FROM production.session_tokens WHERE "token" = $1`;
+  const data = await client.query(queryInsert, [sessionToken]);
+  console.log(data.rows[0]["userid"])
 };
 
 exports.removeDate = (req, res) => {
